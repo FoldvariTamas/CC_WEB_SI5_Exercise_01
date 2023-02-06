8d3f6eb7f795d11d6b2ddbd6840c3a1725ef804c
Erik <erik.raboczki98@gmail.com>
2023-02-06 10:57:10 +0100

    Changed the backend port to 3001

--- Backend/index.js
+++ Backend/index.js
@@ -18,8 +18,8 @@ app.use(express.json())
 //Connect to DB and start the app on port:3000
 Mongoose.connect("mongodb://localhost:27017").then(() => {
     console.log("MongoDB connection established!");
-    app.listen(3000, ()=> {
-        console.log("Backend running on port: 3000!");
+    app.listen(3001, ()=> {
+        console.log("Backend running on port: 3001!");
     })
 }).catch((error) => {
     console.log("Attempt to connect to MongoDB FAILED!");

1d05912f313d501cc788dfe47d64096edd4294ee
Erik <erik.raboczki98@gmail.com>
2023-02-06 10:58:42 +0100

    Changed the backend MongoDB connection string

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 //Connect to DB and start the app on port:3000
-Mongoose.connect("mongodb://localhost:27017").then(() => {
+Mongoose.connect("mongodb://localhost:27017/questions_and_answers").then(() => {
     console.log("MongoDB connection established!");
     app.listen(3001, ()=> {
         console.log("Backend running on port: 3001!");
